fb29e5f0be5cc70e42caaa4a5379aad0c8e612e8
Ольга Костенко <you@example.com>
2024-11-11 02:36:32 +0500

    Создана директория tests с модулями conftest и test_product

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.product import Product, Category
+
+
+@pytest.fixture
+def product():
+    return Product(
+        name="Samsung Galaxy S23 Ultra", description="256GB, Серый цвет, 200MP камера", price=180000.0, quantity=5
+    )
+
+
+@pytest.fixture
+def category():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        products=[
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
+    )
