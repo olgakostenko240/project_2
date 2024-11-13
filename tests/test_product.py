f7f1650a94e53862570608230ce9abc0550a2e76
Ольга Костенко <you@example.com>
2024-11-13 20:00:14 +0500

    Тест для класса category перенесен в модуль test_category

--- tests/test_product.py
+++ tests/test_product.py
@@ -3,15 +3,3 @@ def test_product_init(product):
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
-
-
-def test_category(category):
-    assert category.name == "Смартфоны"
-    assert (
-        category.description
-        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    )
-    assert len(category.products) == 3
-
-    assert category.product_count == 3
-    assert category.category_count == 1
