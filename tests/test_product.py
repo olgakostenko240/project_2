9a3339ae6b13709b4d1718723683d38ec78b0fd9
Ольга Костенко <you@example.com>
2024-11-17 01:15:38 +0500

    Добавлены тесты к новым функционалом класса

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,23 @@
+from src.product import Product
+
+
 def test_product_init(product):
     assert product.name == "Samsung Galaxy S23 Ultra"
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+
+def test_product_price():
+    produkt = Product.new_product(product_list={"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    produkt.name = "Samsung Galaxy S23 Ultra"
+    produkt.description = "256GB, Серый цвет, 200MP камера"
+    produkt.price = 180000.0
+    produkt.quantity = 5
+
+
+def test_product_price_setter(capsys, product):
+    product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
