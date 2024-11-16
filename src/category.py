de9c9537a6b2d649af42a549b3621c6b771fb5de
Ольга Костенко <you@example.com>
2024-11-17 01:10:51 +0500

    Добавлены методы класса

--- src/category.py
+++ src/category.py
@@ -1,4 +1,4 @@
-#from src.product import Product
+from src.product import Product
 
 
 class Category:
@@ -8,15 +8,50 @@ class Category:
     product_count = 0
     category_count = 0
 
+
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.product_count += len(products)
         Category.category_count += 1
 
 
-# if __name__ == "__main__":
+    def add_product(self, new_product: Product):
+        self.__products.append(new_product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+
+    @property
+    def products_in_list(self):
+        return self.__products
+
+
+#if __name__ == "__main__":
+#product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#category1 = Category(
+#"Смартфоны",
+#"Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#[product1, product2, product3]
+#)
+#
+#print(category1.products)
+#product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#category1.add_product(product4)
+#print(category1.products)
+#print(category1.product_count)
+#print(category1.category_count)
 #product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
