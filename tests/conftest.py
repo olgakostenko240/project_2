856e4c0fbb1799e678fe9da8633f965929a01a62
Ольга Костенко <you@example.com>
2024-11-19 23:06:21 +0500

    Добавлены фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import pytest
 from src.product import Product
 from src.category import Category
 from src.product_iterator import ProductIterator
+from src.smartphone_product import Smartphone
+from src.lawn_grass_product import LawnGrass
 
 
 @pytest.fixture
@@ -49,3 +51,26 @@ def product_2():
 @pytest.fixture
 def product_iterator(category):
     return ProductIterator(category)
+
+
+@pytest.fixture
+def product_smartphone_1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                         "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def product_smartphone_2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+
+@pytest.fixture
+def product_lawn_grass_1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def product_lawn_grass_2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
+
