be72265b773ce27390fe67fd4da952cb9b77c7a7
Ольга Костенко <you@example.com>
2024-12-01 23:29:49 +0500

    Добавлен метод

--- src/category.py
+++ src/category.py
@@ -35,3 +35,9 @@ class Category:
     @property
     def products_in_list(self):
         return self.__products
+
+    def middle_price(self):
+        try:
+            return sum([product.price for product in self.__products]) / len(self.__products)
+        except ZeroDivisionError:
+            return 0
